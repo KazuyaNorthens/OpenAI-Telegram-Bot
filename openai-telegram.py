d927674e0d0bc3400c223ce1beaedd9fee5edf8b
ElementaryKid <142092748+KazuyaNorthens@users.noreply.github.com>
2023-08-20 07:48:49 +0700

    Update openai-telegram.py

--- openai-telegram.py
+++ openai-telegram.py
@@ -4,8 +4,8 @@
 import openai
 from aiogram import Bot, Dispatcher, types, executor
 
-bot_tkn = 'TOKEN_BOT_TELEGRAM_ANDA'
-openai.api_key = 'KUNCI_API_OPENAI_ANDA'
+bot_tkn = '6559416952:AAE6aZgxVs9P0p_SCYonAqdUvK7e4xCBxPg'
+openai.api_key = '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'
 
 bot = Bot(token=bot_tkn)
 dp = Dispatcher(bot=bot)
